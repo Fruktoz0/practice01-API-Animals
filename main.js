95d13c7244f5742a53bf861895f326d18159e02f
aarver <bacsajozseff@gmail.com>
2022-05-16 15:03:36 +0200

    CSS fájl módosítása + main.js megírva

---
+++ main.js
@@ -0,0 +1,35 @@
+const result = document.querySelector(".result");
+
+
+async function animallist(){
+
+    const response = await fetch("https://zoo-animal-api.herokuapp.com/animals/rand/10");
+
+    const animals = await response.json();
+
+    let output = "";
+
+    animals.map((animal)=>{
+
+        output +=`
+
+            <div class="animals">
+                <div>
+                    <img src="${animal.image_link}" alt="animals" />
+                </div>
+
+                <h2>${animal.name}</h2>
+                <p class="latin">Latin neve: ${animal.latin_name}</p>
+                <p>Táplálkozása: ${animal.diet}</p>
+                <p>Élőhelye: ${animal.geo_range}<p>
+            </div>
+        
+        `;
+
+    })
+
+    result.innerHTML = output;
+
+}
+
+animallist();
